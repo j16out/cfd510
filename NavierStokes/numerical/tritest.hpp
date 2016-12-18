17dc8e57a40474b8b2d7dc811f575afb2e3e4b65
j16out <avrocf105@live.com>
2016-12-18 10:44:32 -0800

    block thomson

---
+++ NavierStokes/numerical/tritest.hpp
@@ -0,0 +1,23 @@
+#ifndef tritest_INCLUDED
+#define tritest_INCLUDED
+
+
+#include <math.h>
+#include <stdio.h>
+#include "numerical.hpp"
+
+
+#define MAXSIZE (200+2)
+
+
+
+
+void solve_block_thomas(crow & r1, int NRows);
+
+
+
+#endif
+
+
+
+//
